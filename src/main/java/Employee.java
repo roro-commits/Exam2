a5175223a42fdfe6d5ea160f27df2f010d12bb4a
roro-commits <Cidaay5kelop*>
2021-03-10 10:13:05 +0000

    commit

---
+++ src/main/java/Employee.java
@@ -0,0 +1,58 @@
+
+
+public class Employee {
+    private String employeeName;
+    private String  employeeID;
+
+
+
+  public  Employee() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public  Employee(String name, String iD) {
+
+        setEmployeeID(iD);
+        setEmployeeName(name);
+
+    }
+
+
+
+
+    public String getEmployeeName() {
+
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        if (employeeName.length() >= 5)
+        {
+            this.employeeName = employeeName;
+
+        }
+        else
+        {
+            System.out.println("The name length is too short");
+        }
+
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+
+        if (employeeID.length() >= 5)
+        {
+            this.employeeID = employeeID;
+
+        }
+        else
+        {
+            System.out.println("The ID length is too short");
+        }
+
+    }
+}
---
+++ src/main/java/EmployeeList.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+public class EmployeeList {
+
+    private ArrayList<Employee> employeeLIst = new ArrayList<Employee>();
+    private ArrayList<String> iD = new ArrayList<String>();
+
+    //function adding employee to array list
+    public void addEmployee(Employee obj){
+        String uniqueID = obj.getEmployeeID();
+
+            if(!iD.contains(uniqueID))
+            {
+                iD.add(uniqueID);
+                employeeLIst.add(obj);
+
+            }
+            else
+            {
+                System.out.println("Duplicate Employee details found, no object added");
+            }
+
+
+
+
+    }
+    //function removing employee from array list
+    public void deleteEmployee(Employee obj){
+        String uniqueID = obj.getEmployeeID();
+
+        if(!iD.contains(uniqueID))
+        {
+            iD.add(uniqueID);
+            employeeLIst.add(obj);
+
+        }
+        else
+        {
+            System.out.println("Duplicate Employee details found, no object added");
+        }
+
+
+
+
+    }
+
+
+   public  int getLIstSize() {
+         int size;
+        size = employeeLIst.size();
+        return size;
+    }
+
+}
